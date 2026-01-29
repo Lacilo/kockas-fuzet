fe2dc09de1f5d02c2a4d2dac23a8ff95a2bf445f
Lacilo <szaszfail@kkszki.hu>
2026-01-29 09:06:01 +0100

    Add project files.

---
+++ ConsoleApp1/ConsoleApp1/Models/Szolgaltato.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1.Models
+{
+    internal class Szolgaltato
+    {
+        string rovidNev;
+        string nev;
+        string ugyfelszolg;
+        string cim;
+        int tel;
+
+        public string RovidNev { get => rovidNev; set => rovidNev = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Ugyfelszolg { get => ugyfelszolg; set => ugyfelszolg = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public int Tel { get => tel; set => tel = value; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# kockas-fuzet
