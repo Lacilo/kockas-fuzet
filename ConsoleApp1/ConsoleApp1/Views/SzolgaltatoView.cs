9adc964377e0420a58f5253e13d49b136c91c210
Lacilo <szaszfail@kkszki.hu>
2026-01-29 09:33:24 +0100

    javitas

---
+++ ConsoleApp1/ConsoleApp1/Views/SzolgaltatoView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using ConsoleApp1.Models;
+
+public class SzolgaltatoView
+{
+	public SzolgaltatoView()
+	{
+
+	}
+
+	public void ShowSzolgaltatas(Szolgaltato szolgaltato)
+	{
+		Console.WriteLine($"Rövid név:{szolgaltato.RovidNev}");
+		Console.WriteLine($"Név: {szolgaltato.Nev}");
+		Console.WriteLine($"Ügyfélszolgálat: {szolgaltato}");
+		Console.WriteLine($"Cím: {szolgaltato.Cim}");
+	}
+
+	public void ShowSzolgaltatoList(List<Szolgaltato> szolgaltatok)
+	{
+
+	}
+}
